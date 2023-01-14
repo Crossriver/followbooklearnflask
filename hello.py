e64c19d491391f88c9cd5e6075eab86833356d42
Crossriver <dujiang_xjtu@outlook.com>
2023-01-14 18:46:31 +0800

    put all local file to github

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask import make_response
+from flask import redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    # name=None
+    name=['a','b','v','d']
+    return render_template('user.html', comments=name)
+
+
+@app.route('/test/ee')
+def newresp():
+    response = make_response('<h2> hello world2 <h2>')
+    response.set_cookie('answer', '42')
+    return response
+
+
+@app.route('/fff')
+def index2():
+    return redirect('http://www.baidu.com')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
