d86677070fe383f0ef717b32e14f27a476bc8f89
Crossriver <dujiang_xjtu@outlook.com>
2023-01-14 20:50:58 +0800

    3.3charpter

--- hello.py
+++ hello.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template
 from flask import make_response
 from flask import redirect
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
     return render_template('extendsdemo.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
 @app.route('/user/<name>')
 def user(name):
     # name=None
-    name=['a','b','v','d']
-    return render_template('user.html', comments=name)
+    # name=['a','b','v','d']
+    return render_template('user2.html', name=name)
 
 
 @app.route('/test/ee')
