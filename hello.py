cc176d890c29a7887cb15b02c0c2e7757b4f07d5
Crossriver <dujiang_xjtu@outlook.com>
2023-01-17 20:33:07 +0800

    4章节

--- hello.py
+++ hello.py
@@ -1,19 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, flash
 from flask import make_response
 from flask import redirect
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('base_bootstrap.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('looks like you have changed you name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.route('/user/<name>')
 def user(name):
